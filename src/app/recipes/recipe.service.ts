2e49989262aab1468facc21293475cbada0273b5
RudraGowda M Patil <rudragowda53@gmail.com>
2023-07-23 16:29:34 +0000

    Added ingredients to recipe model and ui

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,4 +1,5 @@
 import { EventEmitter } from '@angular/core';
+import { Ingredient } from '../shared/models/ingredient.model';
 import { Recipe } from './recipe.model';
 
 export class RecipeService {
@@ -6,12 +7,14 @@ export class RecipeService {
     new Recipe(
       'A Test Recipe',
       'This is simply a test',
-      'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg'
+      'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg',
+      [new Ingredient('Ing1', 10), new Ingredient('Ing2', 1)]
     ),
     new Recipe(
       'Another Test Recipe',
       'This is simply a test',
-      'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg'
+      'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg',
+      [new Ingredient('Ing3', 5), new Ingredient('Ing4', 3)]
     ),
   ];
   recipeSelected = new EventEmitter<Recipe>();
